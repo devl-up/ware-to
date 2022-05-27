a64353fe7eea8ecade5db20560dccd313fecd249
= <=>
2022-05-27 14:04:36 +0200

    #1

---
+++ backend/src/Presentation/Program.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
---
+++ backend/test/Infrastructure.Tests/InfrastructureTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace Infrastructure.Tests;
+
+public class InfrastructureTests
+{
+    [Fact]
+    public void Test_Should_BeTrue()
+    {
+        Assert.True(true);
+    }
+}
