167803270e08baf1465cf25f615e3f9fb777e7ec
devl-up <devlup.srl@gmail.com>
2022-05-30 11:39:07 +0200

    add global error handling

--- backend/src/Presentation/Program.cs
+++ backend/src/Presentation/Program.cs
@@ -1,10 +1,14 @@
 using Application.Extensions;
 using Infrastructure.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options => options.AddPolicy("All", corsBuilder => corsBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
-builder.Services.AddControllers();
+builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
+builder.Services.AddControllers(options => { options.Filters.Add<ExceptionActionFilter>(); });
+
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
