4e039397bc2006248c4c4580d73713357f7bf2c8
= <=>
2022-05-30 10:24:21 +0200

    implement add product endpoint

--- backend/src/Presentation/Program.cs
+++ backend/src/Presentation/Program.cs
@@ -1,12 +1,16 @@
+using Application.Extensions;
 using Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options => options.AddPolicy("All", corsBuilder => corsBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 builder.Services.AddControllers();
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseCors("All");
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
