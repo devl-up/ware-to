ccca459e89472a4b296865b46ab037e3e7d8c2f7
= <=>
2022-05-30 10:24:04 +0200

    implement add product command

---
+++ backend/src/Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+    }
+}
