453b6845d94d147a3a50215252778c216cab3eeb
devl-up <devlup.srl@gmail.com>
2022-05-30 11:38:57 +0200

    Change add product dto validation

---
+++ backend/src/Application/Attributes/RequiredGuidAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Attributes;
+
+public class RequiredGuidAttribute : RequiredAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is Guid guid)
+        {
+            return guid != Guid.Empty;
+        }
+
+        return base.IsValid(value);
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is Guid guid)
+        {
+            return guid != Guid.Empty ? null : new ValidationResult(ErrorMessage, new[] {validationContext.MemberName}!);
+        }
+
+        return base.IsValid(value, validationContext);
+    }
+}
