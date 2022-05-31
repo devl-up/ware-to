3165a802a9559e811641f10cd0d6a37dcdfbc9b6
devl-up <95089107+devl-up@users.noreply.github.com>
2022-05-31 12:59:29 +0200

    As a Developer, I want migration to run automatically when in development mode (#17)
    * add ef core database hosted service
    
    * remove individual CI

---
+++ backend/src/Infrastructure/EntityFrameworkCore/Services/EfCoreDatabaseService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Infrastructure.EntityFrameworkCore.Services;
+
+internal sealed class EfCoreDatabaseService : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public EfCoreDatabaseService(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await using var scope = _serviceProvider.CreateAsyncScope();
+        var hostEnvironment = scope.ServiceProvider.GetRequiredService<IHostEnvironment>();
+
+        if (!hostEnvironment.IsDevelopment())
+        {
+            return;
+        }
+
+        var dbContext = scope.ServiceProvider.GetRequiredService<WareToDbContext>();
+        await dbContext.Database.MigrateAsync(stoppingToken);
+    }
+}
